eff76879aefaee8f59eec60c6b8ea84b0f43036e
tony <wuls@falcon.(none)>
2014-05-17 22:26:18 +0800

    create directories and basic files.

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
 <?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+require './system/view.php';
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
-
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+$p = $_GET['p'];
+var_dump($p);
+View::show('index');
